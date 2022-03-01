b32832fef3a9d4c00693c807ae4d77d231093431
shubhamjindal30 <shubhamjindal1234@gmail.com>
2022-03-01 21:36:53 +1100

    feat: login page design

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,8 +1,98 @@
-import React from 'react';
-import { Typography } from '@material-ui/core';
+import React, { useState } from 'react';
+import {
+  Button,
+  Container,
+  Grid,
+  makeStyles,
+  Paper,
+  TextField,
+  Typography
+} from '@material-ui/core';
+import { withRouter } from 'react-router-dom';
 
 const Login: React.FunctionComponent = () => {
-  return <Typography>Login page.</Typography>;
+  const classes = useStyles();
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setEmail(e.currentTarget.value);
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) =>
+    setPassword(e.currentTarget.value);
+
+  const handleLogin = () => {};
+  return (
+    <div className={classes.container}>
+      <Container maxWidth="xs">
+        <Paper className={classes.paper}>
+          <Grid container direction="column" alignItems="flex-start" justify="center">
+            <Typography className={classes.heading} variant="h5">
+              Sign in
+            </Typography>
+            <TextField
+              className={classes.emailInput}
+              required
+              fullWidth
+              variant="outlined"
+              label="Email"
+              value={email}
+              autoFocus
+              onChange={handleEmailChange}
+            />
+            <TextField
+              className={classes.passwordInput}
+              required
+              fullWidth
+              variant="outlined"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+            />
+            <Button
+              className={classes.loginBtn}
+              fullWidth
+              variant="contained"
+              color="primary"
+              onClick={handleLogin}
+            >
+              Sign in
+            </Button>
+          </Grid>
+        </Paper>
+      </Container>
+    </div>
+  );
 };
 
-export default Login;
+export default withRouter(Login);
+
+const useStyles = makeStyles(() => ({
+  container: {
+    height: '90vh',
+    backgroundColor: '#fafafa',
+    paddingLeft: '2vw',
+    paddingRight: '2vw',
+    paddingTop: '10vh'
+  },
+  paper: {
+    padding: 40,
+    paddingLeft: 32,
+    paddingRight: 32,
+    borderRadius: 10
+  },
+  heading: {
+    fontWeight: 'bold'
+  },
+  emailInput: {
+    marginTop: 30
+  },
+  passwordInput: {
+    marginTop: 20
+  },
+  loginBtn: {
+    marginTop: 20
+  }
+}));
