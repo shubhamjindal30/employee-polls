aa00315d5c7d954127c1fb56bd05e97109f66504
shubhamjindal30 <shubhamjindal1234@gmail.com>
2022-03-07 23:00:28 +1100

    feat: add tests for _saveQuestion

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,41 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { _saveQuestion } from './store/_DATA';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const testUser = {
+  id: 'tylermcginnis',
+  password: 'abc321',
+  name: 'Tyler McGinnis',
+  avatarURL: '/assets/avatars/tyler.jpg',
+  answers: {
+    vthrdm985a262al8qx3do: 'optionOne',
+    xj352vofupe1dqz9emx13r: 'optionTwo'
+  },
+  questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do']
+};
+
+describe('_saveQuestion()', () => {
+  const newQuestion = {
+    optionOneText: 'True',
+    optionTwoText: 'False'
+  };
+  it('returns new question when correct data is passed', async () => {
+    const response = await _saveQuestion({ author: testUser.id, ...newQuestion });
+    expect(response).toMatchObject({
+      id: expect.any(String),
+      author: testUser.id,
+      optionOne: {
+        text: newQuestion.optionOneText,
+        votes: []
+      },
+      optionTwo: {
+        text: newQuestion.optionTwoText,
+        votes: []
+      }
+    });
+  });
+
+  it('throws error when incorrect data is passed', async () => {
+    await expect(_saveQuestion(newQuestion)).rejects.toEqual(
+      'Please provide optionOneText, optionTwoText, and author'
+    );
+  });
 });
