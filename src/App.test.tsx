3d1b840b7eff9c6e8bb365bcb55acf32a1e008ab
shubhamjindal30 <shubhamjindal1234@gmail.com>
2022-03-07 23:00:51 +1100

    feat: add tests for _saveQuestionAnswer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,4 +1,5 @@
-import { _saveQuestion } from './store/_DATA';
+// @ts-nocheck
+import { _saveQuestion, _saveQuestionAnswer } from './store/_DATA';
 
 const testUser = {
   id: 'tylermcginnis',
@@ -10,6 +11,20 @@ const testUser = {
     xj352vofupe1dqz9emx13r: 'optionTwo'
   },
   questions: ['loxhs1bqm25b708cmbf3g', 'vthrdm985a262al8qx3do']
+};
+
+const testQuestion = {
+  id: '8xf0y6ziyjabvozdd253nd',
+  author: 'sarahedo',
+  timestamp: 1467166872634,
+  optionOne: {
+    votes: ['sarahedo'],
+    text: 'Build our new application with Javascript'
+  },
+  optionTwo: {
+    votes: [],
+    text: 'Build our new application with Typescript'
+  }
 };
 
 describe('_saveQuestion()', () => {
@@ -36,6 +51,26 @@ describe('_saveQuestion()', () => {
   it('throws error when incorrect data is passed', async () => {
     await expect(_saveQuestion(newQuestion)).rejects.toEqual(
       'Please provide optionOneText, optionTwoText, and author'
+    );
+  });
+});
+
+describe('_saveQuestionAnswer()', () => {
+  const newQuestionAnswer = {
+    qid: testQuestion.id,
+    answer: 'optionOne'
+  };
+  it('returns users & questions when correct data is passed', async () => {
+    const response = await _saveQuestionAnswer({ authedUser: testUser.id, ...newQuestionAnswer });
+    expect(response.users[testUser.id].answers[testQuestion.id]).toEqual(newQuestionAnswer.answer);
+    expect(response.questions[testQuestion.id][newQuestionAnswer.answer].votes).toContain(
+      testUser.id
+    );
+  });
+
+  it('throws error when incorrect data is passed', async () => {
+    await expect(_saveQuestionAnswer(newQuestionAnswer)).rejects.toEqual(
+      'Please provide authedUser, qid, and answer'
     );
   });
 });
